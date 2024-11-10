3cc764ea57e5c2deb0e06dad705e706808393294
Brendan Golden <git@brendan.ie>
2024-11-10 23:17:00 +0000

    feat: add nix flake infrastructure

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "CS4125: Lab04";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        # find these here https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=python312Packages.mkdocs
+        packages = [
+          pkgs.python3
+        ] ++ (with pkgs.python3Packages; [
+          pkgs.python3
+          numpy
+          pandas
+          scikit-learn
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "CS4125: Project";
+          packages = packages;
+        };
+      }
+    );
+}
